51c007519c483672ad6c83f9b4f33ffad9af722f
VaL Doroshchuk <valkov@yandex.ru>
2015-01-15 22:07:10 +0200

    Update Failed.php

--- core/frames/Failed.php
+++ core/frames/Failed.php
@@ -7,7 +7,14 @@
 
 namespace Bowling\Frame;
 
+/**
+ * If in two tries, a user fails to knock all pins down.
+ */
 class Failed extends Frame {
+    /**
+     * @param int Count of knocked down pins in first try.
+     * @param int Count of knocked down pins in second try. 
+     */
     function __construct($first, $second) {
         parent::__construct($first, $second);
     }
